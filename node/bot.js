38c72905158d5937916e7bc9abed58f59ddea991
Christie Stringer <Christie.stringer@healthlink.net>
2017-01-19 15:36:49 +1300

    slightly broken

--- node/bot.js
+++ node/bot.js
@@ -1,36 +1,18 @@
 console.log("The bot is starting");
 
 var Twit = require('twit');
+var Yelp = require('yelp');
+
 var config = require('./config');
+var yelpconfig = require('./yelpconfig');
 
+var yelp = new Yelp(yelpconfig);
 var T = new Twit(config);
-
-//function getLocation(){
-//    var params = {
-//    }
-//        q: '#RestaurantLocation',
-//        count: 10
-//    }
-//
-//    T.get('search/tweets', params, gotData);
-//
-//    function gotData(err, data, response) {
-//        var tweets = data.statuses;
-//        for (var i = 0; i < tweets.length; i++){
-//          console.log(tweets[i].text);
-//        }
-//    }
-//}
-
-
 
 var stream = T.stream('user');
 stream.on('tweet', tweetEvent);
 
-function tweetEvent(eventMsg){
-//    var fs = require ('fs');
-//    var json = JSON.stringify(eventMsg,null,2);
-//    fs.writeFile("tweet.json", json);
+function tweetEvent(eventMsg) {
 
     var replyto = eventMsg.in_reply_to_screen_name;
     var text = eventMsg.text;
@@ -38,13 +20,46 @@ function tweetEvent(eventMsg){
     console.log(text);
 
     var location = separateTweet(text);
+    list = search();
+    // for( var i = 0 ; i <= list.length -1; i++){
+    //     console.out(list[i]);
+   // }
 
-    if(replyto === 'christieupskill'){
-        var newtweet = '@' + from + ' your tweet ' + location;
-        tweetIt(newtweet);
-    }
+    //console.log("length of list at tweetEvent: " + listRest.length);
+
+//    if(replyto === 'christieupskill'){
+//        var newtweet = '@' + from + ' restaurants in your area ' ;
+//        tweetIt(newtweet);
+//    }
 }
-
+function search(){
+    yelp.search({ term: 'food', location: 'Montreal' }), (data)=>{
+        allData = data;
+        var food = data.businesses;
+        var list = [];
+        for(var i = 0; i< food.length; i++){
+            list.push(food[i].name);
+            console.log(food[i].name);
+        }
+        for(var j= 0; j< list.length; j++){
+            console.log(list[j]);
+        }
+        if(list!=undefined) {
+            return list;
+        }, (err) => {
+            console.error(err);
+        }
+    );
+}
+function getList(data){
+    var food = data.businesses;
+    var list = [];
+    for(var i = 0; i< food.length; i++){
+        list.push(food[i].name);
+        console.log(food[i].name);
+    }
+    return list;
+}
 function separateTweet(txt){
     return (txt.substr(txt.indexOf(' ')+1));
 }
@@ -52,7 +67,7 @@ function separateTweet(txt){
 function tweetIt(txt){
     var tweet = {
         status: txt
-    }
+    };
 
     T.post('statuses/update', tweet, tweeted);
 
@@ -62,6 +77,36 @@ function tweetIt(txt){
         }else{
         console.log("It worked!");
         }
-//      console.log(data)
     }
 }
+
+
+//
+// function getRestaurants(location){
+//     var bigData = '';
+//     // var params = {
+//     //     term: 'food',
+//     //     location: location
+//     // }
+//
+//     yelp.search({ term: 'food', location: 'Montreal' })
+//         .then(function (data) {
+//             console.log(data);
+//         })
+//     gotData(bigData);
+//
+//     console.log('list at getRest: '+ gotData.length);
+//     return gotData;
+// }
+//
+// function gotData(data){
+//     var restaurants = data.businesses;
+//     console.log(restaurants.length);
+//     var listRest = [];
+//     for(var i = 0; i< restaurants.length; i++){
+//         // console.log(restaurants[i].name);
+//         listRest.push(restaurants[i].name);
+//     }
+//     console.log('list at GotData: '+ listRest.length);
+//     return listRest;
+//}
---
+++ node/yelpconfig.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key: 'lFqjUomhUTWxw3ou3T1H1A',
+    consumer_secret: 'X_E2Vmf0vaf86DPzPjtXizrwdeE',
+    token: '6P6eiGA7NROCmgVhW_P1IDpsroAfujQE',
+    token_secret: '57PgU-BmQvjVbYZLQ77LWmQXQrY',
+}
